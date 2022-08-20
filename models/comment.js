60b366218eb61eb5b65de7b57eb4867bdcb2c9e0
JJooonji <korewa9@naver.com>
2022-08-21 00:43:45 +0900

    파일명 통일을 위한 이름 수정

--- models/comment.js
+++ models/comment.js
@@ -3,17 +3,18 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Comments extends Model {
+  class Comment extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
+
       // define association here
     }
   }
-  Comments.init({
+  Comment.init({
     commentId: {
       allowNull: false,
       primaryKey: true,
@@ -26,7 +27,7 @@ module.exports = (sequelize, DataTypes) => {
     comment: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'Comments',
+    modelName: 'Comment',
   });
-  return Comments;
+  return Comment;
 };
